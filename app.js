c4174112e9d4ee8fd86693ce24f944e9d327ed4d
geovanna <vannasousa28@gmail.com>
2025-10-20 06:46:30 -0300

    Servidor criado

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 'use strict'
+
+async function mostrarPoster (){
+    const container = document.getElementById("caixa")
+    const filme = document.getElementById('input').value.toUpperCase()
+
+    const urls = await buscarPoster()
+
+    container.replaceChildren() //Limpar antes de inserir
+
+    urls.forEach(url => { 
+        const a = document.createElement("a")
+       
+        const img = document.createElement("img")
+        img.classList.add('poster')
+
+        img.src = url
+        a.href = img.src
+        a.appendChild(img)
+        container.appendChild(a)
+        
+    });
+}
+
+
+
+
+// npm install express axios dotenv cors
+
+
